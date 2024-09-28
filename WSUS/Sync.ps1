31ee1d579c31fbde813cef0211ca3aa5a5c31d9a
Mark Wilbrink <markwilbrink86@gmail.com>
2024-09-28 17:36:42 +0200

    Add sync script

---
+++ WSUS/Sync.ps1
@@ -0,0 +1,31 @@
+<#
+	.DESCRIPTION
+	Sync WSUS with Windows Updates every second tuesday and the day after. You can schedule this script and let it run every day.
+
+	.NOTES
+	Author: Mark Wilbrink
+	Date: see Git info
+#>
+
+# Start logging
+Try { Start-Transcript -Path "${PSCommandPath}.log" | Out-Null } Catch {}
+
+$Second_Tuesday = (0..30 | % { (Get-Date -Day 1).AddDays($_) } | ? { $_.DayOfWeek -match "Tuesday|Dinsdag" })[1]
+$Day_After_Second_Tuesday = $Second_Tuesday.AddDays(1)
+
+# Check if today is the second tuesday of the month or the day after
+If ((Get-Date -Format "yyyy-MM-dd") -eq (Get-Date $Second_Tuesday -Format "yyyy-MM-dd") -or (Get-Date -Format "yyyy-MM-dd") -eq (Get-Date $Day_After_Second_Tuesday -Format "yyyy-MM-dd"))
+{
+	Write-Host "Get latest sync status..." -ForegroundColor Yellow
+	(Get-WsusServer).GetSubscription().GetLastSynchronizationInfo()
+
+	Write-Host "Sync with Windows Updates..." -ForegroundColor Yellow
+	(Get-WsusServer).GetSubscription().StartSynchronization()
+}
+Else
+{
+	Write-Host "Today there are no updates available :)" -ForegroundColor Yellow
+}
+
+# Stop logging
+Try { Stop-Transcript | Out-Null } Catch {}
