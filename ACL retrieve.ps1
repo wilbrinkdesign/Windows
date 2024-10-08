171d49fb5c4fa7b090598e0ef7d80306057178b0
Mark Wilbrink <98471282+wilbrinkdesign@users.noreply.github.com>
2024-10-08 15:47:05 +0200

    PSObject to PSCustomObject

--- ACL retrieve.ps1
+++ ACL retrieve.ps1
@@ -22,12 +22,13 @@ Foreach ($File in $Directory)
 	$File_Members = ($File_Path | Get-Acl | select -ExpandProperty Access).IdentityReference
 	$File_Date = $File.LastWriteTime
 
-	$List = New-Object -TypeName PSObject
-	$List | Add-Member -NotePropertyName Path -NotePropertyValue $File_Path
-	$List | Add-Member -NotePropertyName Owner -NotePropertyValue $File_Owner
-	$List | Add-Member -NotePropertyName Permissions -NotePropertyValue $File_Permissions
-	$List | Add-Member -NotePropertyName Access -NotePropertyValue $File_Members
-	$List | Add-Member -NotePropertyName Date -NotePropertyValue $File_Date
+	$List = [PSCustomObject]@{
+		Path        = $File_Path
+		Owner       = $File_Owner
+		Permissions = $File_Permissions
+		Access      = $File_Members
+		Date        = $File_Date
+	}
 
 	$Complete_List += $List
 }
