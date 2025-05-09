54fb4ed056698d9795d73cdbc291d560a6ecb5d3
Mark Wilbrink <markwilbrink86@gmail.com>
2025-05-09 10:14:40 +0200

    Add tab

--- Photos add date metadata.ps1
+++ Photos add date metadata.ps1
@@ -8,7 +8,7 @@
 
 	Dependencies: ExifTool
 
-    Check current metadata date: exiftool -DateTimeOriginal -s3 <file>
+	Check current metadata date: exiftool -DateTimeOriginal -s3 <file>
 
 	.LINK
 	https://exiftool.org/
@@ -22,14 +22,14 @@ $Files = Get-ChildItem $Folder -Recurse
 
 Foreach ($File in $Files)
 {
-    Try
-    {
-        Write-Host "Checking: $($File.Name)" -ForegroundColor Yellow
-        $Name = $($File.Name).Substring(0, 8)
-        $Date = [datetime]::ParseExact($Name, "yyyyMMdd", $null)
-        $File_Date = Get-Date $Date -Format "yyyy:MM:dd HH:mm:Ss"
+	Try
+	{
+		Write-Host "Checking: $($File.Name)" -ForegroundColor Yellow
+		$Name = $($File.Name).Substring(0, 8)
+		$Date = [datetime]::ParseExact($Name, "yyyyMMdd", $null)
+		$File_Date = Get-Date $Date -Format "yyyy:MM:dd HH:mm:Ss"
 
-        Write-Host "Add date $File_Date to metadata of file: $($File.Name)" -ForegroundColor Green
-        exiftool "-DateTimeOriginal=$File_Date" "-DateTimeDigitized=$File_Date" "-overwrite_original" "$($File.FullName)"
-    } Catch {}
+		Write-Host "Add date $File_Date to metadata of file: $($File.Name)" -ForegroundColor Green
+		exiftool "-DateTimeOriginal=$File_Date" "-DateTimeDigitized=$File_Date" "-overwrite_original" "$($File.FullName)"
+	} Catch {}
 }
