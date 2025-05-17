3c425211a4e52256d0cc20c3538e68b2306b2116
Mark Wilbrink <markwilbrink86@gmail.com>
2025-05-17 10:55:14 +0200

    Delete function

--- Backup files and folders.ps1
+++ Backup files and folders.ps1
@@ -13,57 +13,42 @@
 
 	.EXAMPLE
 	PS> <script_name>.ps1 -Source <source> -Destination <destination>
-
-	You can also create a profile.ps1 file, for example: C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1
-	Then add the following to this script (edit your own script en data locations):
-
-	. "<script_name>.ps1"
-
-	Function backup
-	{
-		Backup-Data -Source <source> -Destination <destination>
-	}
-
-	Now every time you start PowerShell, you can run the 'backup' command, and this will fire up this backup script :)
 #>
 
-Function Backup-Data
+Param(
+	[string]$Source,
+	[string]$Destination
+)
+
+$Exclude_Dirs = ".git", ".svn"
+$Exclude_Files = "desktop.ini", "Personal Vault.lnk"
+
+# Check for the source
+If (!(Test-Path -Path $Source))
 {
-	Param(
-		[string]$Source,
-		[string]$Destination
-	)
-
-	$Exclude_Dirs = ".git", ".svn"
-	$Exclude_Files = "desktop.ini", "Personal Vault.lnk"
-
-	# Check for the source
-	If (!(Test-Path -Path $Source))
+	Do
 	{
-		Do
-		{
-			$Source = Read-Host "What would you like to backup? Provide the path"
-		} Until ((Test-Path -Path $Source) -eq $True)
-	}
-
-	# Check for the destination
-	If (!(Test-Path -Path $Destination))
-	{
-		Do
-		{
-			Write-Host ""
-			Write-Host "Available drives:" -ForegroundColor Yellow
-			(Get-Partition | Where-Object DriveLetter -match "[a-z]").DriveLetter
-
-			Write-Host ""
-			$Destination = Read-Host "Where do you want to store the backup? Provide the path"
-		} Until ((Test-Path -Path $Destination) -eq $True)
-	}
-
-	$Destination_Full = "$Destination\$(Get-Date -Format "yyyy-MM-dd")"
-
-	Write-Host "Backup '$Source' to '$Destination_Full'" -ForegroundColor Yellow
-	robocopy $Source $Destination_Full /E /R:0 /MIR /A-:SH /XD $Exclude_Dirs /XF $Exclude_Files
-
-	Write-Host "Don't forget to make an export from your vault!" -ForegroundColor Yellow
+		$Source = Read-Host "What would you like to backup? Provide the path"
+	} Until ((Test-Path -Path $Source) -eq $True)
 }
+
+# Check for the destination
+If (!(Test-Path -Path $Destination))
+{
+	Do
+	{
+		Write-Host ""
+		Write-Host "Available drives:" -ForegroundColor Yellow
+		(Get-Partition | Where-Object DriveLetter -match "[a-z]").DriveLetter
+
+		Write-Host ""
+		$Destination = Read-Host "Where do you want to store the backup? Provide the path"
+	} Until ((Test-Path -Path $Destination) -eq $True)
+}
+
+$Destination_Full = "$Destination\$(Get-Date -Format "yyyy-MM-dd")"
+
+Write-Host "Backup '$Source' to '$Destination_Full'" -ForegroundColor Yellow
+robocopy $Source $Destination_Full /E /R:0 /MIR /A-:SH /XD $Exclude_Dirs /XF $Exclude_Files
+
+Write-Host "Don't forget to make an export from your vault!" -ForegroundColor Yellow
