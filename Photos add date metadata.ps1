8810cd5b639cbf2d5ada78e9aa522ba278061a95
Mark Wilbrink <markwilbrink86@gmail.com>
2025-10-20 19:17:42 +0200

    Add video support

--- Photos add date metadata.ps1
+++ Photos add date metadata.ps1
@@ -46,6 +46,7 @@ Function Photo-MetaDate
 		Do
 		{
 			$Folder = Read-Host "Where are the files that you want to add the date as metadata? Provide the path"
+			$Folder = $Folder.Trim("'`"")
 		} Until ((Test-Path -Path $Folder) -eq $True)
 	}
 
@@ -55,26 +56,41 @@ Function Photo-MetaDate
 
 	Foreach ($File in $Files)
 	{
-		If (!(exiftool -DateTimeOriginal -s3 $File.FullName))
+		Try
 		{
-			Try
-			{
-				Write-Host "Checking: $($File.FullName)" -ForegroundColor Yellow
-				$Name = $($File.Name).Substring(0, 8)
-				$Date = [datetime]::ParseExact($Name, $DateFormat, $null)
-				$File_Date = Get-Date $Date -Format "yyyy:MM:dd HH:mm:Ss"
+			Write-Host "Checking: $($File.FullName)" -ForegroundColor Yellow
+			$Name = $($File.Name).Substring(0, 8)
+			$Date = [datetime]::ParseExact($Name, $DateFormat, $null)
+			$File_Date = Get-Date $Date -Format "yyyy:MM:dd HH:mm:Ss"
 
-				Write-Host "Add date $File_Date to: $($File.FullName)" -ForegroundColor Green
-				exiftool "-DateTimeOriginal=$File_Date" "-DateTimeDigitized=$File_Date" "-overwrite_original" "$($File.FullName)"
-			}
-			Catch
+			If ($File -match ".JPEG|.JPG") # Images
 			{
-				Write-Host "Date not added: $($File.FullName)" -ForegroundColor Red
+				If (!(exiftool -DateTimeOriginal -s3 $File.FullName))
+				{
+					Write-Host "Add date $File_Date to: $($File.FullName)" -ForegroundColor Green
+					exiftool "-DateTimeOriginal=$File_Date" "-DateTimeDigitized=$File_Date" "-overwrite_original" "$($File.FullName)"
+				}
+				Else
+				{
+					Write-Host "Skipping file, date already present: $($File.FullName)" -ForegroundColor Yellow
+				}
+			}
+			Else # Videos
+			{
+				If (!(exiftool -CreateDate -s3 $File.FullName))
+				{
+					Write-Host "Add date $File_Date to: $($File.FullName)" -ForegroundColor Green
+					exiftool "-CreateDate=$File_Date" "-overwrite_original" "$($File.FullName)"
+				}
+				Else
+				{
+					Write-Host "Skipping file, date already present: $($File.FullName)" -ForegroundColor Yellow
+				}
 			}
 		}
-		Else
+		Catch
 		{
-			Write-Host "Skipping file, date already present: $($File.FullName)" -ForegroundColor Yellow
+			Write-Host "Date not added: $($File.FullName)" -ForegroundColor Red
 		}
 	}
 }
